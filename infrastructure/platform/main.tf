c038c93ab9ca86dc6ce642d2677cdf1554a86845
Daniel Bryars <daniel.bryars@neointralogistics.com>
2026-09-19 12:14:18 +0100

    Add passwordless metadata and identity foundation

--- infrastructure/platform/main.tf
+++ infrastructure/platform/main.tf
@@ -3,11 +3,13 @@ resource "google_project_service" "platform" {
     "artifactregistry.googleapis.com",
     "compute.googleapis.com",
     "iam.googleapis.com",
+    "identitytoolkit.googleapis.com",
     "logging.googleapis.com",
     "monitoring.googleapis.com",
     "run.googleapis.com",
     "secretmanager.googleapis.com",
     "serviceusage.googleapis.com",
+    "sqladmin.googleapis.com",
   ])
 
   project            = var.project_id
@@ -40,4 +42,71 @@ resource "google_service_account" "control_plane" {
   display_name = "Kratos control plane"
 
   depends_on = [google_project_service.platform]
+}
+
+resource "google_sql_database_instance" "kratos" {
+  count = var.enable_database ? 1 : 0
+
+  project             = var.project_id
+  name                = "kratos"
+  region              = var.region
+  database_version    = "POSTGRES_16"
+  deletion_protection = var.database_deletion_protection
+
+  settings {
+    tier              = "db-f1-micro"
+    availability_type = "ZONAL"
+    disk_type         = "PD_SSD"
+    disk_size         = 10
+    disk_autoresize   = true
+
+    backup_configuration {
+      enabled                        = true
+      point_in_time_recovery_enabled = true
+    }
+
+    database_flags {
+      name  = "cloudsql.iam_authentication"
+      value = "on"
+    }
+
+    ip_configuration {
+      ipv4_enabled = true
+    }
+  }
+
+  depends_on = [google_project_service.platform]
+}
+
+resource "google_sql_database" "kratos" {
+  count = var.enable_database ? 1 : 0
+
+  project  = var.project_id
+  name     = "kratos"
+  instance = google_sql_database_instance.kratos[0].name
+}
+
+resource "google_sql_user" "control_plane" {
+  count = var.enable_database ? 1 : 0
+
+  project  = var.project_id
+  name     = trimsuffix(google_service_account.control_plane.email, ".gserviceaccount.com")
+  instance = google_sql_database_instance.kratos[0].name
+  type     = "CLOUD_IAM_SERVICE_ACCOUNT"
+}
+
+resource "google_project_iam_member" "control_plane_cloud_sql_client" {
+  count = var.enable_database ? 1 : 0
+
+  project = var.project_id
+  role    = "roles/cloudsql.client"
+  member  = "serviceAccount:${google_service_account.control_plane.email}"
+}
+
+resource "google_project_iam_member" "control_plane_cloud_sql_user" {
+  count = var.enable_database ? 1 : 0
+
+  project = var.project_id
+  role    = "roles/cloudsql.instanceUser"
+  member  = "serviceAccount:${google_service_account.control_plane.email}"
 }
